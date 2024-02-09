a34b9a4f27866fa596e6b3596b0982f8b9860599
Anton-Osip <osipchika@gmail.com>
2024-02-09 12:02:09 +0300

    feat: add basic section header

---
+++ src/pages/main-page/main-page-header/main-page-header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './main-page-header.module.css';
+
+export const MainPageContent: React.FC = () => {
+    return <></>;
+};
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
-import './main-page.css';
+import React from 'react';
+import styles from './main-page.module.css';
+import { MainPageHeader } from './main-page-header/main-page-header';
+import { Layout } from 'antd';
+
+import Sider from 'antd/lib/layout/Sider';
 
 export const MainPage: React.FC = () => {
-    const [collapsed, setCollapsed] = useState(false);
-
-    return <></>;
+    return (
+        <Layout id={styles.mainPage}>
+            <Sider width={208}></Sider>
+            <Layout className={styles.mainPageContent}>
+                <MainPageHeader />
+            </Layout>
+        </Layout>
+    );
 };
