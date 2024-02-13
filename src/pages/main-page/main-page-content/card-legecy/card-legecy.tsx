bc4c5de9fd9d171573b33382661d3018ba6ea50e
Anton-Osip <osipchika@gmail.com>
2024-02-13 13:23:45 +0300

    fix: card-legecy for main-page

--- src/pages/main-page/main-page-content/card-legecy/card-legecy.tsx
+++ src/pages/main-page/main-page-content/card-legecy/card-legecy.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import styles from './card-legecy.module.css';
+import { Card } from 'antd';
+import classNames from 'classnames';
+
+interface IListItem {
+    id: string;
+    text: string;
+}
+
+const listItem: IListItem[] = [
+    {
+        id: 'listItem1',
+        text: '— планировать свои тренировки на календаре, выбирая тип и уровень нагрузки;',
+    },
+    {
+        id: 'listItem2',
+        text: ' — отслеживать свои достижения в разделе статистики, сравнивая свои результаты с нормами и рекордами;',
+    },
+    {
+        id: 'listItem3',
+        text: '— создавать свой профиль, где ты можешь загружать свои фото, видео и отзывы о тренировках;',
+    },
+    {
+        id: 'listItem4',
+        text: ' — выполнять расписанные тренировки для разных частей тела, следуя подробным инструкциям и советам профессиональных тренеров.',
+    },
+];
 
 export const CardLegesy: React.FC = () => {
     return (
-        <div className={`${styles.contentCard} ${styles.contentCardAction}`}>
+        <Card id={styles.contentCard}>
             <p className={styles.contentCardAction__text}>С CleverFit ты сможешь:</p>
             <ul className={styles.contentCardAction__list}>
-                <li className={styles.contentCardAction__items}>
-                    — планировать свои тренировки на календаре, выбирая тип и уровень нагрузки;
-                </li>
-                <li className={styles.contentCardAction__items}>
-                    — отслеживать свои достижения в разделе статистики, сравнивая свои результаты с
-                    нормами и рекордами;
-                </li>
-                <li className={styles.contentCardAction__items}>
-                    — создавать свой профиль, где ты можешь загружать свои фото, видео и отзывы о
-                    тренировках;
-                </li>
-                <li className={styles.contentCardAction__items}>
-                    — выполнять расписанные тренировки для разных частей тела, следуя подробным
-                    инструкциям и советам профессиональных тренеров.
-                </li>
+                {listItem.map((item: IListItem) => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
             </ul>
-        </div>
+        </Card>
     );
 };
